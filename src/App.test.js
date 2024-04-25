1de5b9b117d8b642b7552841b829ec18c56a7d10
shcherbyna-d <shch.d.vol@gmail.com>
2024-04-25 22:13:13 +0300

    Added one basic unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,57 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+  act,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+import { convertToPdf } from "./services/pdfApi";
+import "fake-indexeddb/auto";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("./services/pdfApi", () => ({
+  convertToPdf: jest.fn(),
+}));
+
+//Not the best but fast workaround for IndexedDB
+global.structuredClone = (val) => {
+  return JSON.parse(JSON.stringify(val));
+};
+
+describe("App", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should convert text to PDF when "Convert to PDF" button is clicked', async () => {
+    // Mock URL.createObjectURL
+    global.URL.createObjectURL = jest.fn(() => "blob:mock-url");
+
+    // Arrange
+    const mockPdfBlob = new Blob(["Mock PDF"], { type: "application/pdf" });
+    convertToPdf.mockResolvedValue(mockPdfBlob);
+
+    render(<App />);
+
+    const textInput = screen.getByRole("textbox");
+    const convertButton = screen.getByRole("button", {
+      name: /convert to pdf/i,
+    });
+
+    // Act
+    await act(async () => {
+      fireEvent.change(textInput, { target: { value: "Test text" } });
+      userEvent.click(convertButton);
+    });
+
+    // Assert
+    expect(convertToPdf).toHaveBeenCalledWith("Test text");
+    expect(convertToPdf).toHaveBeenCalledTimes(1);
+
+    // Check if pdf-viewer appear in the DOM
+    const pdfViewer = screen.getByTestId("pdf-viewer");
+    expect(pdfViewer).toBeInTheDocument();
+    expect(pdfViewer.src).toBe("blob:mock-url");
+  });
 });
--- src/components/PdfViewer/PdfViewer.jsx
+++ src/components/PdfViewer/PdfViewer.jsx
@@ -1,7 +1,13 @@
 const PdfViewer = ({ pdfUrl }) => {
   return (
     <div>
-      <embed src={pdfUrl} type="application/pdf" width="100%" height="1100px" />
+      <embed
+        data-testid="pdf-viewer"
+        src={pdfUrl}
+        type="application/pdf"
+        width="100%"
+        height="1100px"
+      />
     </div>
   );
 };
